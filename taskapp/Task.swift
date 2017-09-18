af4651056a97b163dbee94e73fb0b19529110bf6
岸本 諒太 <ryotakishimoto@kishimoto-no-macbook-air.local>
2017-09-18 22:02:28 +0900

    カテゴリーを追加

---
+++ taskapp/Task.swift
@@ -0,0 +1,33 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 岸本 諒太 on 2017/09/14.
+//  Copyright © 2017年 ryota.kishimoto. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    /// カテゴリー
+    dynamic var category = ""
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
